fe1148966517563367670134d40f77d2e4c33c6b
Luis Herrera <herrera.lrh@gmail.com>
2019-10-26 21:21:00 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,11 @@
 def unsafe?(speed)
-if speed > 60 || speed < 40
-	return true
-else
-	return false
-end
+	if speed > 60
+		true
+	end
 end
 
 
 
 def not_safe?(speed)
- speed > 60 || speed < 40 ? true : false
+
 end
